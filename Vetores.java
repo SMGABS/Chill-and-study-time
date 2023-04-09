9c336c124c62f447381a2e0a95736e07cd6e7c33
SMGABS <119013543+SMGABS@users.noreply.github.com>
2023-04-09 17:34:33 -0300

    Starting in module behavior vector,arrays,lists
    this code asks a number of peoples, your heights for calcule the average height between hes.

---
+++ Vetores.java
@@ -0,0 +1,43 @@
+package vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+	public class Vetores {
+	
+	
+	
+		public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner entry = new Scanner(System.in);
+		System.out.println("Insert number of peoples:  ");
+		int n = entry.nextInt();
+		System.out.println();
+		
+		double[] vect = new double[n];//created vetor for n"number of peoples" n = number of heights//
+		
+		
+		for(int i=0; i<n; i++) {
+			int f = 1;
+			int nn = f + i;
+			System.out.printf("Insert Height "+ nn +":  ");//a nn and f variable just created for better format for print.
+			System.out.println();
+			vect[i] = entry.nextDouble();
+
+		}
+		
+		double sum = 0.0;
+		for (int i=0; i<n; i++) {
+		sum += vect[i]; //
+	}
+			double average = sum / n;
+			
+			System.out.print("Average Height: "+ average);
+			
+					
+	entry.close();
+	
+}
+	}
